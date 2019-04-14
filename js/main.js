42a036ed8413df2df6b62c633fd4dbd6a1fe929d
qutji <maritomida@gmail.com>
2019-04-14 09:45:31 +0900

    add table

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 var ctx = document.getElementById("myChart");
 var myChart = new Chart(ctx, {
-  type: 'radar',
+  type: 'polarArea',
   data: {
     labels: ["M", "T", "W", "T", "F", "S", "S"],
     datasets: [{
-      label: 'apples',
-      backgroundColor: "rgba(153,255,51,0.4)",
-      borderColor: "rgba(153,255,51,1)",
+      backgroundColor: [
+        "#2ecc71",
+        "#3498db",
+        "#95a5a6",
+        "#9b59b6",
+        "#f1c40f",
+        "#e74c3c",
+        "#34495e"
+      ],
       data: [12, 19, 3, 17, 28, 24, 7]
-    }, {
-      label: 'oranges',
-      backgroundColor: "rgba(255,153,0,0.4)",
-      borderColor: "rgba(255,153,0,1)",
-      data: [30, 29, 5, 5, 20, 3, 10]
     }]
   }
 });
